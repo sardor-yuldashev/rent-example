7d62d3962cb6e73111b9ab9a7f397a7bb219cd59
sy <syuldashev8135@gmail.com>
2024-01-25 15:09:57 -0500

    added dynamicParams

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -15,6 +15,8 @@ import Heading from "../Heading";
 import Calendar from "../inputs/Calendar";
 import Counter from "../inputs/Counter";
 
+export const dynamicParams = true;
+
 enum STEPS {
     LOCATION = 0,
     DATE = 1,
